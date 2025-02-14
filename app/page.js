7a34a1a7d71cfbf4ccca3c80cf76a7a1d073e4b2
GOKUL <gokuln2004@gmail.com>
2025-02-14 11:19:36 +0530

    project

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import page from "user"
+
+export default function Home() {
+  return (
+    <div>
+      
+      <userDashboard />
+
+    </div>
+  );
+}
---
+++ app/userDashboard/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function page() {
+  return (
+    <View>
+      <Text>page</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
